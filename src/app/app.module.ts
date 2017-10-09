c542868c37b04f59f60b8ee188d38b5c0c83bc2a
Andrew Chen <ac12491@gmail.com>
2017-10-09 17:36:15 -0600

    pics added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PriceServicesComponent } from './price-services/price-services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PriceServicesComponent
   ],
   imports: [
     BrowserModule
