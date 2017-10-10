1374d37fe346f62942a15593f57f309497167797
Andrew Chen <ac12491@gmail.com>
2017-10-10 16:57:34 -0600

    pricing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,24 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
 import { NgModule } from '@angular/core';
 import { routes } from './app.router';
 import { CommonModule } from '@angular/common';
-
+import { ng2Parallax  } from 'ang2-parallax/ng2parallax';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PriceServicesComponent } from './price-services/price-services.component';
 
-import { 
+import {
   MatToolbarModule,
   MatButtonModule
  } from '@angular/material';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    PriceServicesComponent
+    PriceServicesComponent,
+    ng2Parallax
   ],
   imports: [
     BrowserModule,
