0a41731d4fcaf010f3dac650bcb9f2416d2f17d9
Harry Vu <harryvu801@gmail.com>
2017-10-09 18:39:42 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InspectorsComponent } from './inspectors/inspectors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InspectorsComponent
   ],
   imports: [
     BrowserModule
